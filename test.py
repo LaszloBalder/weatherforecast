c52c988594cc060b9901ebef49ce048ceb999e10
László Balder <laszlo.balder@gmail.com>
2018-11-24 19:14:18 +0100

    type

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ x = Forecast(data)
 
 h = x.getFirstHour()
 while h:
-#	print('\tlight: ',h.getLight(), '\tcloud: ', h.getCloudCoverPerc())
+	print('\tlight: ',h.getLight(), '\tcloud: ', h.getCloudCoverPerc())
 	h = x.getNextHour()
 
 with urllib.request.urlopen("https://graphdata.buienradar.nl/forecast/jsonsun/?type=sun&lat=52.4&lon=5.28") as urldata:
